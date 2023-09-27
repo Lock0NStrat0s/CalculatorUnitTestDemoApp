3237d3744364ccb7458e2fa526e5a1acebd4698b
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-27 01:35:23 -0600

    Add project files.

---
+++ CalculationLibrary.Test/OperationsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationLibrary.Test
+{
+    public class OperationsTests
+    {
+        [Theory]
+        public void OperationShouldReturnAddition()
+        {
+            // Arrange
+            string expected 
+        }
+    }
+}
---
+++ CalculationLibrary/Operations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationLibrary
+{
+    public class Operations
+    {
+        public int Add(int uno, int dos)
+        {
+            return uno + dos;
+        }
+
+        public int Subtract(int uno, int dos)
+        {
+            return uno + dos;
+        }
+
+        public int Multiply(int uno, int dos)
+        {
+            return uno + dos;
+        }
+
+        public int Divide(int uno, int dos)
+        {
+            return uno + dos;
+        }
+    }
+}
